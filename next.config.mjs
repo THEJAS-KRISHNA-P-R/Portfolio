e8d05fbebeb907126d30acb400cca40916246e2a
THEJAS-KRISHNA-P-R <thejas2124@gmail.com>
2026-03-04 00:08:47 +0530

    feat: configure Next.js with custom headers and add Vercel configuration

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,20 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  async headers() {
+    return [
+      {
+        source: '/(.*)',
+        headers: [
+          { key: 'Cross-Origin-Embedder-Policy', value: 'require-corp' },
+          { key: 'Cross-Origin-Opener-Policy', value: 'same-origin' },
+        ],
+      },
+    ]
+  },
+  webpack(config) {
+    config.experiments = { ...config.experiments, asyncWebAssembly: true, layers: true }
+    return config
+  },
+};
 
 export default nextConfig;
